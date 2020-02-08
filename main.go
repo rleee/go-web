535af4634e95357408bcef98198783c4a0379c84
rleee <danlee0150@gmail.com>
2020-02-08 15:32:31 +0800

    using if statement to determine the path user on our side

--- main.go
+++ main.go
@@ -12,5 +12,12 @@ func main() {
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Hello there!</h1>")
+	fmt.Fprint(w, "Your current path on out site: ", r.URL.Path, "<br>")
+
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello there!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "Checkout our latest collections at <a href=\"testlink.com\">fotoku.com</a>")
+	}
+
 }
