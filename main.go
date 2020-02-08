fdd861c1fb241592cd0fd462b10ab623f3d0e5f4
rleee <danlee0150@gmail.com>
2020-02-08 16:36:50 +0800

    use gorilla mux router

--- main.go
+++ main.go
@@ -3,25 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":8000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":8000", r)
 }
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	// fmt.Fprint(w, "Your current path on out site: ", r.URL.Path, "<br>")
+	fmt.Fprint(w, "<h1>Hello there!</h1>")
+}
 
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello there!</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "Checkout our latest collections at <a href=\"testlink.com\">fotoku.com</a>")
-	} else {
-		// Header status code will only work if there's no content send to client yet
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>We could not find the page you're looking for :(</h1> Please send us and email to tell us whats wrong.")
-	}
-
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Checkout our latest collections at <a href=\"testlink.com\">fotoku.com</a>")
 }
