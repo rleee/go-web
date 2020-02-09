09b10dd0716cf6a50b174e03451a02530a7a400e
rleee <danlee0150@gmail.com>
2020-02-09 14:15:19 +0800

    refactoring code

--- views/view.go
+++ views/view.go
@@ -1,6 +1,15 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
+
+var (
+	layoutDir   string = "views/layouts/"
+	templateExt string = ".gohtml"
+)
 
 // View will return pointer to parsed template
 type View struct {
@@ -8,13 +17,14 @@ type View struct {
 	Layout   string
 }
 
+// Render is used to render template attached to View type
+func (v View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
+
 // NewView will get parsed template
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/navbar.gohtml",
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/footer.gohtml",
-	)
+	files = append(files, layoutFiles()...) // using ... is to unpack the slice
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -25,4 +35,14 @@ func NewView(layout string, files ...string) *View {
 		Template: t,
 		Layout:   layout,
 	}
+}
+
+// returns all files location string based on
+// variable arguments provided in Glob
+func layoutFiles() []string {
+	files, err := filepath.Glob(layoutDir + "*" + templateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
